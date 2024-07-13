143f3d3ebfbfd4718623471969c269eef3b3a4c0
snehahaaha <sneha.17253@sakec.ac.in>
2024-07-13 16:17:19 +0530

    Add files via upload

---
+++ a.js
@@ -0,0 +1,54 @@
+const bestsellerBtn = document.querySelector('.bestseller-btn');
+const popuupBox = document.querySelector('.popuup-box');
+
+bestsellerBtn.addEventListener('click', () => {
+  if (popuupBox.style.display === "none") {
+    popuupBox.style.display = "block";
+  } else {
+    popuupBox.style.display = "none";
+  }
+});
+
+const skincareBtn = document.querySelector('.skincare-btn');
+const popupBox = document.querySelector('.popup-box');
+
+skincareBtn.addEventListener('click', () => {
+  if (popupBox.style.display === "none") {
+    popupBox.style.display = "block";
+  } else {
+    popupBox.style.display = "none";
+  }
+});
+
+const lipcareBtn = document.querySelector('.lipcare-btn');
+const popuppBox = document.querySelector('.popupp-box');
+
+lipcareBtn.addEventListener('click', () => {
+  if (popuppBox.style.display === "none") {
+    popuppBox.style.display = "block";
+  } else {
+    popuppBox.style.display = "none";
+  }
+});
+
+const buildyourroutineBtn = document.querySelector('.buildyourroutine-btn');
+const popupppBox = document.querySelector('.popuppp-box');
+
+lipcareBtn.addEventListener('click', () => {
+  if (popupppBox.style.display === "none") {
+    popupppBox.style.display = "block";
+  } else {
+    popupppBox.style.display = "none";
+  }
+});
+
+const trackorderBtn = document.querySelector('.trackorder-btn');
+const popuppppBox = document.querySelector('.popupppp-box');
+
+trackorderBtn.addEventListener('click', () => {
+  if (popuppppBox.style.display === "none") {
+    popuppppBox.style.display = "block";
+  } else {
+    popuppppBox.style.display = "none";
+  }
+});
